97d6c6241264960b8a5fb7e0dd76f661bfcecac4
inakicarcereny <inakicarce@hotmail.com.ar>
2024-03-25 16:46:34 -0300

    pics filter project

---
+++ pics-filter/app.js
@@ -0,0 +1,18 @@
+const buscador = document.querySelector('.buscador');
+const imagenes = document.querySelectorAll('.carta');
+
+const filtrador = () => {
+  buscador.addEventListener('keyup', (e) => {
+    const textoBusqueda = e.target.value.toLowerCase();
+    imagenes.forEach((carta) => {
+      const textoCarta = carta.textContent.toLowerCase();
+      if (textoCarta.includes(textoBusqueda)) {
+        carta.classList.remove('filter');
+      } else {
+        carta.classList.add('filter');
+      }
+    });
+  });
+}
+
+filtrador(); // Llama a la función para activar el evento de filtrado
